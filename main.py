8ac2542f843be3049c4c7b67c7c7c0a5117f9c11
Zoey Chapell <zac9557@rit.edu>
2025-09-07 13:53:40 -0400

    Added some super high level pseudocode.

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+# Import the required files
+import llm_client
+import cli_utility
+import sanitize
 
+def main():
+    print('test')
+    # call cli_utility 
+    
+    # This will grab the user prompt
+
+    # then feed it into sanitize and store 
+    # TRACK what is changed 
+
+    # send sanitized content to llm_client and store
+    
+    # grab the llm response
+
+    # fill in any details if needed
+
+    # ouput the response to the user
+
+if __name__ == '__main__':
+    main()
