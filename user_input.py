78f10f392ffe795f52996b08dbd7cf5e7f7789a5
Zoey Chapell <zac9557@rit.edu>
2025-09-09 18:44:06 -0400

    Basic UI (asks for user input) and cleans the data with new lines.

---
+++ user_input.py
@@ -0,0 +1,35 @@
+import sys
+'''
+PrivPrompt
+
+Zoey Chappell, Adam Braccia, Minn Myint
+
+CSEC-490, Rochester Institute of Technology
+'''
+
+    
+'''
+Function that takes in the user input, then cleans the input. 
+Parameters: 
+    - user_input : The string returned from user_input function. 
+Returns: 
+    - cleaned_user_input : A nicely formatted string'''
+def clean_input():
+    # User input gathered.
+    print("Enter your text (Ctrl+Z Enter to finish):")
+    # By using sys.stdin.read(), the program can accept multiple lines of input
+    user_text = sys.stdin.read() # Note, can introduce character limits here with read(#)
+    # Checks for new lines and replaces them with a space.
+    cleaned_user_input = user_text.replace("\n", "").replace("\r", "")
+    # Turn to lower case for better matching.
+    cleaned_user_input.lower()
+    # Return the cleaned input and trims whitespace.
+    return cleaned_user_input.strip()
+
+
+def main(): 
+    user_input = clean_input()
+    print(user_input)
+
+if __name__ == '__main__':
+    main()
