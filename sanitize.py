ec112dd3e5953efc53e5a2b31ba2dd2ea02621f3
Zoey Chapell <zac9557@rit.edu>
2025-09-09 19:46:21 -0400

    Changed regex matching to improve time efficiency and added SSN matching functionality

--- sanitize.py
+++ sanitize.py
@@ -8,26 +8,40 @@ CSEC-490, Rochester Institute of Technology
 import re
 
 def sanitize(user_input): 
+    EMAIL_PATTERN = re.compile(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")
+    SSN_PATTERN_1 = re.compile (r"\d{3}-\d{2}-\d{4}") # ex. 111-12-1234
+    SSN_PATTERN_2 = re.compile(r"\d{3} \d{2} \d{4}") # ex. 111 12 1234
     dict_email = {}
-    # Search for emails
-    matches = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", user_input)
+    dict_ssn = {}
+    '''Email Match'''
+    email_matches = EMAIL_PATTERN.findall(user_input)
     # If an email address is found
-    if matches:
+    if email_matches:
         # Iterate through the email matches
-        for _ in matches: 
-            counter = 1
+        counter = 1
+        for _ in email_matches: 
             # Add the email matches to dict_email and the replacement value
             dict_email[_] = 'user' + str(counter) + '@email.com'
-            counter += 1
+            counter = counter + 1
             # Replace the finding with the replacement. 
-            user_input = re.sub(_, dict_email[_], user_input)
-    else: 
-        print("no emails found.")
+            user_input = user_input.replace(_, dict_email[_])
+ 
+    '''SSN Match'''
+    ssn_matches = SSN_PATTERN_1.findall(user_input) + SSN_PATTERN_2.findall(user_input)
+    if ssn_matches:
+        s_counter = 1
+        for _ in ssn_matches:
+            # Add the SSN and replacement string to the dictionary
+            dict_ssn[_] = f"XXX-XX-{1000+s_counter:04d}"
+            # Replace the found ssns with XXX-XX-100#
+            user_input = user_input.replace(_, dict_ssn[_])
+            s_counter = s_counter + 1
+    # here for testing       
     print (user_input)
     
 
 def main(): 
-    sanitize("This is a test zoey.chappell@rit.edu")
+    sanitize("This is a test zoey.chappell@rit.edu, anothertest@yahoo.com, 123 14 1235 and an ssn 123-12-1234")
 
 if __name__ == '__main__':
     main()
