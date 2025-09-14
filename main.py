029fc6ddf6072fa962266d937472b1577e9732ac
Zoey Chapell <zac9557@rit.edu>
2025-09-14 12:07:46 -0400

    Adding a couple small changes to make the demo a little nicer

--- main.py
+++ main.py
@@ -14,18 +14,16 @@ def main():
     # call clean_input 
     cli_instance = CLI()  # Create instance
     user_input = cli_instance.cli() 
+    print("\033[34mDEMO DETAILS: \n This is the original user input:\033[0m \n")
+    print(user_input)
     #user_input = clean_input()
     # then feed it into sanitize and store 
     # TRACK what is changed 
     user_input_cleaned, dict_email, dict_ssn = sanitize_input(user_input)
+    print ("\033[34mDEMO DETAILS: \n This is the sanitized message:\033[0m \n")
     print(user_input_cleaned)
     # send sanitized content to llm_client and store
     groq(user_input_cleaned)
-    # grab the llm response
-
-    # fill in any details if needed
-
-    # ouput the response to the user
 
 if __name__ == '__main__':
     main()
