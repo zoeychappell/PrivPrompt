7a79d82d616f8c1b64d8e8caa652fa48db1c92ee
Zoey Chapell <zac9557@rit.edu>
2025-09-21 19:25:29 -0400

    implemented and tested stanza for NLP. Started creating a entity ruler for spacy since it struggles with lowercase single names

--- my-flask-app/sanitize.py
+++ my-flask-app/sanitize.py
@@ -1,6 +1,10 @@
 import re
 # for name recognition
 import spacy
+from spacy.pipeline import EntityRuler
+# For human name recognition
+import stanza
+
 
 def sanitize_input(user_input): 
     EMAIL_PATTERN = re.compile(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}") # Matches email addresses 
@@ -13,15 +17,21 @@ def sanitize_input(user_input):
     dict_ssn = {}
     dict_name = {}
 
+    # #####################
     # Emails
+    # #####################
+
     email_matches = EMAIL_PATTERN.findall(user_input)
     counter = 1
     for e in email_matches:
         dict_email[e] = f"user{counter}@email.com"
         user_input = user_input.replace(e, dict_email[e])
         counter += 1
-
+    
+    # #####################
     # SSNs
+    # #####################
+
     ssn_matches = SSN_PATTERN_1.findall(user_input) + SSN_PATTERN_2.findall(user_input) + SSN_PATTERN_3.findall(user_input) + SSN_PATTERN_4.findall(user_input)
     s_counter = 1
     for s in ssn_matches:
@@ -29,23 +39,70 @@ def sanitize_input(user_input):
         user_input = user_input.replace(s, dict_ssn[s])
         s_counter += 1
 
+    # #####################
     # Names
+    # #####################
+
     # Load a pre-trained English model (you might need to download it first: python -m spacy download en_core_web_sm)
     nlp = spacy.load("en_core_web_sm")
+    ruler = nlp.add_pipe('entity_ruler', before="ner")
+    patterns = [{"label": "PERSON", "pattern": name} for name in ['zoey', 'john']]
+    ruler.add_patterns(patterns)
 
+    # Note: spacy has a displaCy visualizer 
     text = user_input
     doc = nlp(text)
-
+    
     person_names = []
     for ent in doc.ents:
         if ent.label_ == "PERSON":
             person_names.append(ent.text)
     n_counter = 1
     for n in person_names: 
+        if n == 'Email' or n == 'Volunteer':
+            person_names.remove(n)
         dict_name[n] = f"name{n_counter}"
         user_input = user_input.replace(n, dict_name[n])
         n_counter = n_counter + 1
     print(dict_name)
+    '''
+    Note from zoey: 
+    I'm currently testing out solutions for name recognition. 
+    Stanza is slower and tends seems to be about the same as spaCy
+    so i'm not sold yet 
+    --------------------------
+    nlp_stanza = stanza.Pipeline('en')
+    doc_stanza = nlp_stanza("Zoey Is A Person, so is Barack Obama")
+    print(doc_stanza.entities)
+    '''
     return user_input, dict_email, dict_ssn, dict_name
 
+
+'''
+Fail cases found: 
+    O'Connor, O'Malley fails
+    - o'malley works?
+        omalley works
+    
+    - fails with single names too
+
+    have an issue with not separating names out
+    {"george O'malley george omalley": 'name1'}
+    {'zoey adam kirby riley caitlin maleah': 'name1'}
+
+
+    'XXX-XX-1004': 'name11'}
+
+    Russian names fail: 
+        Dmitry Иванов
+
+    French names fail
+        Léon Dupont
+
+    Japense names normally succeed but wiht a few failures
+        Kobayashi Riku
+
+    Chinese names normally succeed but occasionally fail
+        Lui Fang
+'''
 
