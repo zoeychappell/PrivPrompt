a622db8988ca34e2051e969cb818fe580c312954
Minn Myint <mtm7465@rit.edu>
2025-09-26 12:14:13 -0400

    added json file writing for pii dictionaries (QA testing)

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from livereload import Server
 from sanitize import sanitize_input
 from llm_clients.groq_llm_client import call_groq
+# import json
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # disable caching
@@ -28,10 +29,11 @@ def handle_prompt():
 
     # UNCOMMENT CODE FOR QA TESTING
     # Requires the text1, text2, and textresponse .txt files in ./PrivPrompt/ (can reconfigure it as you like)
+    # Also requires the pii_data.json file. Uncomment import json when using this file
     # Must run app.py inside the ./PrivPrompt/ folder (can reconfigure it as you like)
     #   python my-flask-app/app.py
     #
-    # Appends both responses to textresponse.txt with 5 newlines between (empty file once in a while)
+    # # Appends both responses to textresponse.txt with 5 newlines between (empty file once in a while)
     # with open("../PrivPrompt/textresponse.txt", "a", encoding="utf-8") as f:
     #     f.write(ai_response_original + "\n" * 5 + ai_response_sanitized + "\n" * 5)
 
@@ -41,6 +43,14 @@ def handle_prompt():
 
     # with open("../PrivPrompt/text2.txt", "w", encoding="utf-8") as f2:
     #     f2.write(ai_response_sanitized)
+
+    # # Writes the dictionaries of collected PII into the pii_data.json file
+    # with open("../PrivPrompt/pii_data.json", "w", encoding="utf-8") as f:
+    #     json.dump({
+    #         "emails": dict_email,
+    #         "ssns":   dict_ssn,
+    #         "names":  dict_name
+    #     }, f, indent=2)
 
 
     return jsonify({
